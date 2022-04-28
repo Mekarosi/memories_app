85267ab7d9e78a49cd60a8dce72b6dae0597c532
Mekarosi <mekarosi_assa@yahoo.com>
2022-04-28 13:18:12 -0700

    Create post model and post routes

--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import config from 'config'
 
+import postRoutes from './routes/posts.js'
 
 const app = express()
+
+
+
+
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = config.get('mongoURL')
 const PORT = process.env.PORT || 5000
