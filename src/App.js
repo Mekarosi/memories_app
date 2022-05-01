a4fbd9ed2539abdb9e0bf0fea916c05b3db56baa
Mekarosi <mekarosi_assa@yahoo.com>
2022-05-01 07:26:01 -0700

    Create components and styled appBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import Form from './components/Form/Form'
+import Posts from './components/Posts/Posts'
 
-function App() {
+import memories from './images/memories.PNG'
+
+import useStyles from './styles'
+
+const App = () => {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <Typography className={classes.heading} variant="h2" align='center'>Memories</Typography>
+            <img className={classes.image} src={memories} alt='memories' height='60'/>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+   </Container>
+  )
 }
 
 export default App;
