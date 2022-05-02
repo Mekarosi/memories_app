455e8663ab9cee5217eb1947acf87d14a690fbc4
Mekarosi <mekarosi_assa@yahoo.com>
2022-05-02 02:24:14 -0700

    Connect redux to app

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 
+import useStyles from './styles'
+
+
 const Post = () => {
+    const classes = useStyles()
     return (
         <h1>POST</h1>
     )
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,7 +2,12 @@ import React from 'react'
 import Post from './Post/Post'
 
 
+
+import useStyles from './styles'
+ 
+
 const Posts = () => {
+  const classes = useStyles()
     return (
         <>
           <h1>POSTS</h1>
