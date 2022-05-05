c3857a96ad3195b25bcbf07e0173428b501c3f9a
Mekarosi <mekarosi_assa@yahoo.com>
2022-05-05 10:07:57 -0700

    Add features to like post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,9 +4,9 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
-import { deletePost } from '../../../actions/posts'
+import { deletePost, likePost } from '../../../actions/posts'
 
 import useStyles from './styles'
 
@@ -37,7 +37,7 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant='h5' gutterBottom >{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize='small'/>
                     Like
                     {post.likeCount}
