659371512301d6569ecbfacf5ef2dc397eb15d65
Mekarosi <mekarosi_assa@yahoo.com>
2022-05-22 10:43:23 -0700

    Refactor App component

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,9 @@ import Auth from './components/Auth/Auth'
 import PostDetails from './components/PostDetails/PostDetails'
 
 const App = () => {
- 
+  
+  const user = JSON.parse(localStorage.getItem('profile'))
+
   return (
     <BrowserRouter>
       <Container maxWidth="xl">
@@ -19,7 +21,7 @@ const App = () => {
            <Route  path='/posts' exact component={Home} />
            <Route  path='/posts/search' exact component={Home} />
            <Route path='/posts/:id' component={PostDetails} />
-           <Route path='/auth' exact component={Auth} />   
+           <Route path='/auth' exact component={() => (!user ? <Auth /> : <Redirect  to='/posts' />)} />   
          </Switch>   
       </Container>
     </BrowserRouter>
